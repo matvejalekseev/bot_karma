6ee88181d1527d626324ebec5a6d536d28f4b572
m.alekseev <alekseevmatvej@gmail.com>
2018-11-20 16:47:21 +0300

    new feature

--- freceiver.py
+++ freceiver.py
@@ -19,4 +19,4 @@ def json_example():
     return resp
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host="http://195.201.136.255")
+    app.run(debug=True, port=5000, host="195.201.136.255")
