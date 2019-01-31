dcc19f4514a198356ecd2f4babd49d727cdf82e7
m.alekseev <alekseevmatvej@gmail.com>
2019-01-31 09:22:39 +0300

    new feature

--- receiver.py
+++ receiver.py
@@ -1,12 +1,27 @@
 from flask import Flask, request, Response
+from conf import HOST_RECEIVER
 
 app = Flask(__name__)
 
 @app.route('/sign', methods=['POST'])
-def json_example():
+def sign():
     resp = Response(request.data.decode('utf-8'))
     resp.headers = request.headers
     return resp
 
+
+@app.route('/check_length', methods=['POST'])
+def check_length():
+    resp = Response(request.data.decode('utf-8'))
+    resp.headers.clear()
+    for header in request.headers:
+        if header[0] == 'Content-Length':
+            resp.headers.add(header[0], int(header[1])-200)
+        else:
+            resp.headers.add(header[0], header[1])
+    print(resp.data.decode('utf-8'))
+    return resp
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host="195.201.136.255")
+    app.run(port=5000, host=HOST_RECEIVER)
